935654a823549c8593cd0e6816ec582f787b77d9
Vaisakh Murali <mvaisakh@statixos.com>
2026-05-12 13:09:38 +0530

    CNote: build: consolidate QML module registration
    - Unified NoteCanvas and FileManager registration under the CeriumNotes module.
    - Removed manual qmlRegisterType calls in favor of build-system automation.
    - Fixed 'type not found' error by ensuring consistent URI metadata generation.
    - Cleaned up CMakeLists.txt to follow modern Qt 6 QML module standards.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@ int main(int argc, char *argv[])
     Cerium::setupCrashHandler();
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<FileManager>("CeriumNotes", 1, 0, "FileManager");
-
     app.setApplicationName("CeriumNotes");
     app.setOrganizationName("CeriumNotes");
 
